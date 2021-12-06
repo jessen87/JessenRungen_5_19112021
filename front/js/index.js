0aac25e934b57cf807c2db510d1dadf29347641f
Jess <jessen.rungen87@gmail.com>
2021-12-06 10:23:27 +0100

    recuperation des données clients

--- front/js/index.js
+++ front/js/index.js
@@ -1,28 +1,48 @@
-async function getItems() {
-	try {
-		let response = await fetch("http://localhost:3000/api/products");
-		return await response.json();
-	} catch (error) {
-		console.log("Error : " + error);
-	}
+fillSection();
+
+// Récupération des articles de l'API
+async function getArticles() {
+    var articlesCatch = await fetch("http://localhost:3000/api/products")
+    return await articlesCatch.json();
 }
 
-// Handle the render on the HTML
-(async function renderItems() {
-	let items = await getItems();
-	let htmlRender = "";
-	items.forEach((item) => {
-		let htmlContent = `
-		<a href="./product.html?id=${item._id}">
-			<article>
-				<img src="${item.imageUrl}" alt="${item.altTxt}">
-				<h3 class="productName">${item.name}</h3>
-				<p class="productDescription">${item.description}</p>
-			</article>
-		</a>
-		`;
-		htmlRender += htmlContent;
-	});
-	const itemContainer = document.getElementById("items");
-	itemContainer.innerHTML += htmlRender;
-})();
+    // Répartition des données de l'API dans le DOM
+async function fillSection() {
+    var result = await getArticles ()
+    .then(function (resultatAPI){
+        const articles = resultatAPI;
+        console.table(articles);
+        for (let article in articles) {
+
+            // Insertion de l'élément "a"
+            let productLink = document.createElement("a");
+            document.querySelector(".items").appendChild(productLink);
+            productLink.href = `product.html?id=${resultatAPI[article]._id}`;
+
+            // Insertion de l'élément "article"
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
+
+            // Insertion de l'image
+            let productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = resultatAPI[article].imageUrl;
+            productImg.alt = resultatAPI[article].altTxt;
+
+            // Insertion du titre "h3"
+            let productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = resultatAPI[article].name;
+
+            // Insertion de la description "p"
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productName");
+            productDescription.innerHTML = resultatAPI[article].description;
+        }
+    })
+    .catch (function(error){
+        return error;
+    });
+}
